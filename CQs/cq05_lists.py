bfd53f48200839daf91ac2c3b4dcd24aed8e8e68
Brianna Burnett <bub@unc.edu>
2024-10-04 15:49:01 -0400

    challenge question 5 work

--- CQs/cq05_lists.py
+++ CQs/cq05_lists.py
@@ -3,4 +3,20 @@
 __author__ = "730695410"
 
 
-def manual_append(list[int]: int) -> None:
+def manual_append(int_list: list[int], value: int) -> None:
+    int_list.append(value)
+
+
+def double(int_list: list[int]) -> None:
+    index: int = 0
+    while index < len(int_list):
+        int_list[index] = int_list[index] * 2
+        index += 1
+
+
+list_1: list[int] = [1, 2, 3]
+list_2: list[int] = list_1
+
+double(list_2)
+print(list_1)
+print(list_2)

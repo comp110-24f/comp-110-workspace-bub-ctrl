10cc14db83fad0f572c43dfc4492d2ca710774ff
Brianna Burnett <bub@unc.edu>
2024-10-06 16:16:00 -0400

    exercise four almost finished

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -18,13 +18,28 @@ def max(input: list[int]) -> int:
     """Returns the largest int in the list"""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    index: int = 0
-    largest_int: int = input[index]
+    index: int = 1
+    largest_int: int = input[0]
     while index < len(input):
-        if input[index] > largest_int:
-            largest_int = input[index]
+        if largest_int > input[index]:
             index += 1
         else:
+            largest_int = input[index]
             index += 1
-
     return largest_int
+
+
+def is_equal(int_list_1: list[int], int_list_2: list[int]) -> bool:
+    index: int = 0
+    if len(int_list_1) != len(int_list_2):
+        return False
+    while index < len(int_list_1):
+        if int_list_1[index] == int_list_2[index]:
+            index += 1
+        else:
+            return False
+    return True
+
+
+def extend(int_list_1: list[int], int_list_2: list[int]) -> None:
+    int_list_1.append(int_list_2)

0b6dcd05eb9e56d7194c55d4cbc5835098966762
Brianna Burnett <bub@unc.edu>
2024-11-18 17:58:29 -0500

    completed ex08

--- exercises/ex08/linked_list.py
+++ exercises/ex08/linked_list.py
@@ -4,17 +4,19 @@ from __future__ import annotations
 
 
 class Node:
+    """Defining the Node class."""
+
     value: int
     next: Node | None
 
     def __init__(self, value: int, next: Node | None):
+        """Initializing Node."""
         self.value = value
         self.next = next
 
     def __str__(self) -> str:
-        """Produce a string representation of a linked list"""
+        """Produce a string representation of a linked list."""
         rest: str = "TODO"
-        # TODO: Figure out the rest of the list
         if self.next is None:
             rest = "None"
         else:
@@ -23,40 +25,46 @@ class Node:
 
 
 def value_at(head: Node | None, index: int) -> int:
-    if head is None:
+    """Returns value of Node at index."""
+    if head is None:  # index error
         raise IndexError("Index is out of bounds on the list.")
 
-    if index == 0:
+    if index == 0:  # base case
         return head.value
-
-    else:
+    else:  # recursive case
         return value_at(head.next, index - 1)
 
 
 def max(head: Node | None) -> int:
-    if head is None:
+    """Returns maximum value in the linked list."""
+    if head is None:  # value error
         raise ValueError("Cannot call max with None.")
 
-    max_value: int = 0
+    if head.next is None:  # if only one value in linked list
+        return head.value  # that is highest value
 
-    if head.value > max_value:
-        max_value = head.value
+    max_value: int = max(head.next)  # recursive call on max_value
 
-    if head.next is None:
-        return max_value
+    if head.value > max_value:  # if node value is greater than current max
+        max_value = head.value  # current max is node value
 
-    else:
-        return max(head.next)
+    return max_value  # ultimately returning an int
 
 
 def linkify(items: list[int]) -> Node | None:
-    if len(items) == 0:
+    """Returns linked list of Nodes with same values."""
+    start: int = 0
+    end: int = len(items)  # if len(items) == 0:
+    if start == end:  # if list is empty, return none
         return None
     else:
-        x: str = str(linkify(items[1 : len(items)]))
-        __str__(x)
+        return Node(items[0], linkify(items[1:]))  # splice operator [start:end]
 
 
 def scale(head: Node | None, factor: int) -> Node | None:
-
-    return factor * scale(head.value, factor)
+    """Return new linked list of Nodes multiplied by given factor."""
+    if head is None:  #
+        return None
+    return Node(
+        head.value * factor, scale(head.next, factor)
+    )  # multiplies each value by the given factor
